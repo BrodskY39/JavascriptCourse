2dfdc60bad4a97501b2b30c8bda7001d33e6ac2f
BrodskY39 <brodskayajane@gmail.com>
2024-11-05 22:59:18 +0200

    Add files via upload

---
+++ Lesson 1.2.js
@@ -0,0 +1,11 @@
+let firstName = 'Yevheniia';
+console.log(firstName);
+
+let middleName = 'Yuriivna';
+console.log(middleName);
+
+let lastName = 'Brodska';
+console.log(lastName);
+
+let msg = firstName + ' ' + middleName + ' ' + lastName;
+console.log(msg);

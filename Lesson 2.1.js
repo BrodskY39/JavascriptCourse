34f877114611abcca28d4a8b3236f34cce2ff485
BrodskY39 <brodskayajane@gmail.com>
2024-11-07 22:22:29 +0200

    Add files via upload

---
+++ Lesson 2.1.js
@@ -0,0 +1,121 @@
+let arr;
+
+arr = [15, 2, 38, -79, 14, 404, 212, -119, 999, 12];
+console.log(arr);
+
+console.log(arr[0]);
+console.log(arr[1]);
+console.log(arr[2]);
+console.log(arr[3]);
+console.log(arr[4]);
+console.log(arr[5]);
+console.log(arr[6]);
+console.log(arr[7]);
+console.log(arr[8]);
+console.log(arr[9]);
+
+// Task #LARqoUj5I
+
+let book = {
+    title: 'Pride and Prejudice',
+    pageCount: 279,
+    genre: 'Roman'
+}
+console.log(book);
+
+let book2 = {
+    title: 'The Master and Margarita',
+    pageCount: 361,
+    genre: 'Satiric Roman'
+}
+console.log(book2);
+
+let book3 = {
+    title: 'Schindler Ark',
+    pageCount: 242,
+    genre: 'Historical Roman'
+}
+console.log(book3);
+
+// Task #sA3Gg1sCp
+
+let book4 = {
+    title: 'The Master and Margarita',
+    pageCount: 361,
+    genre: 'Satiric Roman',
+    authors: [
+        {name: 'Michael Bulhakov',
+        age: 49},
+        {name: 'Michael Bulhakov',
+            age: 49}
+    ]
+}
+console.log(book4);
+
+let book5 = {
+    title: 'Schindler Ark',
+    pageCount: 361,
+    genre: 'Historical Roman',
+    authors: [
+        {name: 'Thomas Keneally',
+        age: 37},
+        {name: 'Thomas Keneally',
+            age: 37},
+    ]
+}
+console.log(book5);
+
+let book6 = {
+    title: 'The Girl with the Dragon Tattoo',
+    pageCount: 298,
+    genre: 'Detectiv',
+    authors: [
+        {name: 'Larsson Stieg',
+        age: 51},
+        {name: 'Larsson Stieg',
+            age: 51}
+    ]
+}
+console.log(book6);
+
+// Task #jCHFnEbdmFd
+
+let user1 = {name: 'Vasya', username: 'Bolt', password: '51616516'};
+let user2 = {name: 'Olya', username: 'Volya', password: '8549498'};
+let user3 = {name: 'Lena', username: 'Vip', password: '165165548'};
+let user4 = {name: 'Kseniya', username: 'Gossip', password: '99191959'};
+let user5 = {name: 'Alex', username: 'Sokol', password: '2778277'};
+let user6 = {name: 'Alex', username: 'Sokol', password: '2787587272'};
+let user7 = {name: 'Alex', username: 'Sokol', password: '857827722'};
+let user8 = {name: 'Alex', username: 'Sokol', password: '272727722'};
+let user9 = {name: 'Alex', username: 'Sokol', password: '867287282'};
+let user10 = {name: 'Alex', username: 'Sokol', password: '3873893933'};
+console.log(user1.password);
+console.log(user2.password);
+console.log(user3.password);
+console.log(user4.password);
+console.log(user5.password);
+console.log(user6.password);
+console.log(user7.password);
+console.log(user8.password);
+console.log(user9.password);
+console.log(user10.password);
+
+// Task #coYydZuaeEB
+
+let temps = [
+    {day: 'Sunday', values:
+            {Morningtemp: 11, DayTemp: 22, NightTemp: 12}},
+    {day: 'Monday', values:
+            {Morningtemp: 12, DayTemp: 20, NightTemp: 14}},
+    {day: 'Tueday', values:
+            {Morningtemp: 11, DayTemp: 21, NightTemp: 13}},
+    {day: 'Wednesday', values:
+            {Morningtemp: 13, DayTemp: 21, NightTemp: 14}},
+    {day: 'Thursday', values:
+            {Morningtemp: 10, DayTemp: 22, NightTemp: 14}},
+    {day: 'Friday', values:
+            {Morningtemp: 11, DayTemp: 23, NightTemp: 11}},
+    {day: 'Saturday', values:
+            {Morningtemp: 11, DayTemp: 21, NightTemp: 12}}
+]
---
+++ Lesson 2.2.js
@@ -0,0 +1,148 @@
+let k = '???';
+if (k!==0) {
+    console.log('right');
+}
+else {
+    console.log('wrong');
+}
+
+// Task #3ckURgvs
+
+let time = 17;
+if (time >= 0 && time < 15) {
+    console.log('1 quater');
+}
+else if (time >= 16 && time < 30) {
+    console.log('2 quater');
+}
+else if (time >= 31 && time < 45) {
+    console.log('3 quater');
+}
+else if (time >= 46 && time < 60) {
+    console.log('4 quater');
+}
+
+// Task #UMoNq4biWGe
+
+let day = 12
+if (day >=1 && day <=10){
+    console.log('1 декада');
+} else if (day > 10 && day <= 20) {
+    console.log('2 декада');
+} else if (day > 10 && day <= 20) {
+    console.log('2 декада');
+}
+
+// Task #KzrtqyQ
+
+let key = '4'
+switch (key){
+    case '1' :
+        console.log('8.00 Morning excercise');
+        console.log('8.30 Breakfast');
+        console.log('09.00 Work');
+        console.log('13.00 Coffee break');
+        console.log('14.00 Work');
+        console.log('18.00 Dinner');
+        break;
+    case '2' :
+        console.log('8.00 Morning excercise');
+        console.log('8.30 Breakfast');
+        console.log('09.00 Work');
+        console.log('13.00 Coffee break');
+        console.log('14.00 Work');
+        console.log('18.00 Dinner');
+        break;
+    case '3' :
+        console.log('8.00 Morning excercise');
+        console.log('8.30 Breakfast');
+        console.log('09.00 Work');
+        console.log('13.00 Coffee break');
+        console.log('14.00 Work');
+        console.log('18.00 Dinner');
+        break;
+    case '4' :
+        console.log('8.00 Morning excercise');
+        console.log('8.30 Breakfast');
+        console.log('09.00 Work');
+        console.log('13.00 Coffee break');
+        console.log('14.00 Work');
+        console.log('18.00 Dinner');
+        break;
+    case '5' :
+        console.log('8.00 Morning excercise');
+        console.log('8.30 Breakfast');
+        console.log('09.00 Work');
+        console.log('13.00 Coffee break');
+        console.log('14.00 Work');
+        console.log('18.00 Dinner');
+        break;
+    case '6' :
+        console.log('8.00 Morning excercise');
+        console.log('8.30 Breakfast');
+        console.log('09.00 Work');
+        console.log('13.00 Coffee break');
+        console.log('14.00 Work');
+        console.log('18.00 Dinner');
+        break;
+    case '7' :
+        console.log('8.00 Morning excercise');
+        console.log('8.30 Breakfast');
+        console.log('09.00 Work');
+        console.log('13.00 Coffee break');
+        console.log('14.00 Work');
+        console.log('18.00 Dinner');
+        break;
+    default:
+        console.log('???????????');
+}
+
+// Task #uwsz1RnTQJ1
+
+let a = 8;
+let b = 2;
+if (a>b) {
+    console.log(a);
+}
+else if (b>a) {
+    console.log(b);
+}
+else if (a === b) {
+    console.log('===');
+}
+
+// Task #iBvqtjEm
+
+let x = false;
+if (!x) {
+    x = 'default';
+}
+
+// Task #awLXL6TBzg
+
+let coursesAndDurationArray = [
+    {title: 'JavaScript Complex', monthDuration: 5},
+    {title: 'Java Complex', monthDuration: 6},
+    {title: 'Python Complex', monthDuration: 6},
+    {title: 'QA Complex', monthDuration: 4},
+    {title: 'FullStack', monthDuration: 7},
+    {title: 'Frontend', monthDuration: 4}
+];
+if (coursesAndDurationArray[0].monthDuration > 5) {
+    console.log('Super');
+}
+if (coursesAndDurationArray[1].monthDuration > 5) {
+    console.log('Super');
+}
+if (coursesAndDurationArray[2].monthDuration > 5) {
+    console.log('Super');
+}
+if (coursesAndDurationArray[3].monthDuration > 5) {
+    console.log('Super');
+}
+if (coursesAndDurationArray[4].monthDuration > 5) {
+    console.log('Super');
+}
+if (coursesAndDurationArray[5].monthDuration > 5) {
+    console.log('Super');
+}
